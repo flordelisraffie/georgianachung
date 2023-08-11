5f995b3f1888585b404ce01dc4c3c0c1aa1526ee
Raffie Flordelis <raffieflordelis@elink.corp>
2023-08-12 02:30:28 +0800

    duplicate func

--- functions.php
+++ functions.php
@@ -14,3 +14,24 @@ add_action( 'wp_enqueue_scripts', function(){
     wp_enqueue_script('custom-script', get_stylesheet_directory_uri() . '/js/divi-child-script.js', array( 'jquery' ));
     
 });
+
+
+//phpmailer
+add_action( 'phpmailer_init', function($phpmailer){
+
+	if ( ! is_object( $phpmailer ) ) {
+            $phpmailer = (object) $phpmailer;
+      }
+
+    $phpmailer->Mailer     = 'smtp';
+	$phpmailer->Host       = 'mxserver.readersmagnet.com';
+	$phpmailer->SMTPAuth   =  true;
+	$phpmailer->Port       = '465';
+	$phpmailer->Username   = 'author@georgianapchung.com';
+	$phpmailer->Password   = 'Chung@1234';
+	$phpmailer->SMTPSecure = 'ssl';
+	$phpmailer->From       = 'wordpress@georgianapchung.com';
+	$phpmailer->FromName   = 'georgianapchung.com';
+	$phpmailer->Subject   = 'From georgianapchung.com';
+
+});
